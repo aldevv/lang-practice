fe812750de7e995d3e3fa018c6757b1a299f76cb
Alejandro Bernal <jbernalxyz@gmail.com>
2024-01-18 22:02:41 -0500

    added c webserver

---
+++ c/webserver/cwebserver.c
@@ -0,0 +1,88 @@
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+// maximum application buffer
+#define APP_MAX_BUFFER 1024
+#define PORT 8080
+
+int main() {
+  // define the server and client file descriptors
+  int server_fd, client_fd;
+
+  // define the socket address
+  struct sockaddr_in address;
+  int address_len = sizeof(address);
+
+  // define the application buffer where we receive the requests
+  // data will be moved from receive buffer to here
+  char buffer[APP_MAX_BUFFER] = {0};
+
+  // Create socket
+  // (AF is adress family, INET is ipv4)
+  // SOCK_STREAM, socket will accept streams
+  // 0 means choose the protocol automatically (TCP is a stream protocol)
+  // so probably will choose that
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    perror("Socket failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // Bind socket
+  address.sin_family = AF_INET;         // ipv4
+  address.sin_addr.s_addr = INADDR_ANY; // listen 0.0.0.0 interfaces
+  address.sin_port = htons(PORT);       // host to network short
+
+  if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    perror("Bind failed");
+    exit(EXIT_FAILURE);
+  }
+
+  // Creates the queues
+  // Listen for clients, with 10 backlog (10 connections in accept queue)
+  if (listen(server_fd, 10) < 0) {
+    perror("Listen failed");
+    exit(EXIT_FAILURE);
+  }
+
+  //[C,C,C,C,C,C,C,C,C,C]
+
+  // we loop forever
+  while (1) {
+    printf("\nWaiting for a connection...\n");
+
+    // Accept a client connection client_fd == connection
+    // this blocks the application if accept is empty
+    // if the accept queue is empty, we are stuck here..
+    if ((client_fd = accept(server_fd, (struct sockaddr *)&address,
+                            (socklen_t *)&address_len)) < 0) {
+      perror("Accept failed");
+      exit(EXIT_FAILURE);
+    }
+
+    // read data from the OS receive buffer to the application (buffer)
+    // this is essentially reading the HTTP request
+    // don't bite more than you chew APP_MAX_BUFFER
+    // here's one copy
+    read(client_fd, buffer, APP_MAX_BUFFER);
+    printf("%s\n", buffer);
+
+    // We send the request by writing to the socket send buffer in the OS
+    char *http_response = "HTTP/1.1 200 OK\n"
+                          "Content-Type: text/plain\n"
+                          "Content-Length: 13\n\n"
+                          "Hello world!\n";
+
+    // write to the socket
+    // send queue os
+    write(client_fd, http_response, strlen(http_response));
+
+    // close the client socket (terminate the TCP connection)
+    close(client_fd);
+  }
+
+  return 0;
+}
